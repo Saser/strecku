2e95127d5647504a65cc3a77ee7b82753102e08f
Christian Persson <Saser@users.noreply.github.com>
2019-09-02 10:29:48 +0200

    Begin working on a `tools.mk` file to install all tools
    My initial approach was to keep everything in a single, huge Makefile,
    in order to make it easily searchable. However, this Makefile has
    become difficult to overview and has introduced some problems relating
    to dependencies between tools. As such, I have decided to move
    everything out to a separate `tools.mk` Makefile that contains targets
    and recipes for all tools. This will create clearer separation between
    the installation of a tool, and the invocation of said tool. It will
    also enable the main Makefile to have all tools be declared before
    they are used, solving the problem of dependencies between tools.
    
    In this initial iteration of `tools.mk`, the `gofumports` and
    `golangci-lint` tools are the only tools included, just to show how
    the `tools.mk` file should be structured.

---
+++ tools.mk
@@ -0,0 +1,38 @@
+build/tools/gobin/Makefile:
+	git submodule update --init --recursive $(dir $@)
+include build/tools/gobin/Makefile
+
+# Tools shall be sorted in alphabetical order.
+
+# GOBIN_BINDIR is the top-level directory for where executable binaries should be placed. The actual binaries are placed
+# in subdirectories under GOBIN_BINDIR, based on the tool's name and version.
+GOBIN_BINDIR := build/tools/bin
+
+# `gofumports` is a tool for formatting Go files and insert/removing/sorting import statements. `gofumports` enforces a
+# stricter formatting than `go fmt`.
+GOFUMPORTS_VERSION := 96300e3d49fbb3b7bc9c6dc74f8a5cc0ef46f84b
+GOFUMPORTS_BINDIR := $(GOBIN_BINDIR)/gofumports/$(GOFUMPORTS_VERSION)
+GOFUMPORTS := $(GOFUMPORTS_BINDIR)/gofumports
+$(GOFUMPORTS): $(GOBIN) | $(GOFUMPORTS_BINDIR)
+	GOBIN=$(GOFUMPORTS_BINDIR) $(GOBIN) mvdan.cc/gofumpt/gofumports@$(GOFUMPORTS_VERSION)
+
+# `golangci-lint` is a tool to run numerous linters for Go code.
+GOLANGCI_LINT_VERSION := v1.17.1
+GOLANGCI_LINT_BINDIR := $(GOBIN_BINDIR)/golangci-lint/$(GOLANGCI_LINT_VERSION)
+GOLANGCI_LINT := $(GOLANGCI_LINT_BINDIR)/golangci-lint
+$(GOLANGCI_LINT): $(GOBIN) | $(GOLANGCI_LINT_BINDIR)
+	GOBIN=$(GOLANGCI_LINT_BINDIR) $(GOBIN) github.com/golangci/golangci-lint/cmd/golangci-lint@$(GOLANGCI_LINT_VERSION)
+
+BINDIRS := \
+	$(GOFUMPORTS_BINDIR) \
+	$(GOLANGCI_LINT_BINDIR)
+$(BINDIRS):
+	mkdir --parent '$@'
+
+# tools-all: install all tools. Useful when setting up a new development environment or in CI. The latter advantage
+# comes from the fact that all tools are installed to the same directory (see the `GOBIN_BINDIR` variable above) and
+# thus can easily be cached in a CI environment.
+.PHONY: tools-all
+tools-all: \
+	$(GOFUMPORTS) \
+	$(GOLANGCI_LINT)
