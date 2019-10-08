42159db8ebaef173f63aec6a5b5f0107030e880b
Christian Persson <Saser@users.noreply.github.com>
2019-10-08 08:57:45 +0200

    internal/dockertest: move all methods over to `Client`
    I figured: what is the need for having a separate "pool" structure?
    What I'm providing an abstraction over/convenience methods for is the
    Docker client.
    
    Furthermore, the underlying Docker client is no longer exported. This
    is to avoid confusion as to why the wrapper `Client` both has a
    `ContainerStart` and `StartContainer` method, for example.

--- internal/dockertest/client.go
+++ internal/dockertest/client.go
@@ -1,17 +1,24 @@
 package dockertest
 
 import (
+	"bufio"
+	"context"
 	"fmt"
 	"net/url"
 	"os"
+	"time"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	dockerclient "github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 	"go.uber.org/zap"
 )
 
 type Client struct {
-	*dockerclient.Client
-	Host string
+	dc     *dockerclient.Client
+	logger *zap.Logger
+	host   string
 }
 
 func NewClient(
@@ -39,8 +46,9 @@ func NewClient(
 	}
 	logger.Info("creating dockertest client wrapper", zap.String("host", host))
 	client := &Client{
-		Client: dockerClient,
-		Host:   host,
+		dc:     dockerClient,
+		logger: logger,
+		host:   host,
 	}
 	logger.Info("created dockertest client wrapper")
 	cleanup := func() {
@@ -52,4 +60,112 @@ func NewClient(
 		logger.Info("closed docker client")
 	}
 	return client, cleanup, nil
+}
+
+func (c *Client) PullOfficialImage(ctx context.Context, imageName string, tag string) error {
+	canonicalName := fmt.Sprintf("docker.io/library/%s:%s", imageName, tag)
+	logger := c.logger.With(zap.String("canonicalName", canonicalName))
+	logger.Info("pulling image")
+	reader, err := c.dc.ImagePull(ctx, canonicalName, types.ImagePullOptions{})
+	if err != nil {
+		return fmt.Errorf("pull official image: %w", err)
+	}
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		msg := scanner.Text()
+		logger.Debug(msg)
+	}
+	logger.Info("pulled image")
+	if err := reader.Close(); err != nil {
+		return fmt.Errorf("pull official image: %w", err)
+	}
+	return nil
+}
+
+func (c *Client) StartContainer(ctx context.Context, imageName string, tag string) (string, error) {
+	imageLogger := c.logger.With(
+		zap.String("imageName", imageName),
+		zap.String("tag", tag),
+	)
+	imageLogger.Info(
+		"pulling image of container to run",
+		zap.String("imageName", imageName),
+		zap.String("tag", tag),
+	)
+	if err := c.PullOfficialImage(ctx, imageName, tag); err != nil {
+		return "", fmt.Errorf("start container: %w", err)
+	}
+	imageLogger.Info("image pulled, creating container")
+	containerConfig := &container.Config{
+		Image: fmt.Sprintf("%v:%v", imageName, tag),
+	}
+	hostConfig := &container.HostConfig{
+		PublishAllPorts: true,
+	}
+	createResponse, err := c.dc.ContainerCreate(ctx, containerConfig, hostConfig, nil, "")
+	if err != nil {
+		return "", fmt.Errorf("start container: %w", err)
+	}
+	id := createResponse.ID
+	idLogger := c.logger.With(zap.String("id", id))
+	idLogger.Info(
+		"created container, now starting it",
+	)
+	if err := c.dc.ContainerStart(ctx, id, types.ContainerStartOptions{}); err != nil {
+		return "", fmt.Errorf("start container: %w", err)
+	}
+	idLogger.Info("container started")
+	return id, nil
+}
+
+func (c *Client) StopContainer(ctx context.Context, containerID string, timeout time.Duration) error {
+	idLogger := c.logger.With(zap.String("id", containerID))
+	idLogger.Info("stopping container")
+	if err := c.dc.ContainerStop(ctx, containerID, &timeout); err != nil {
+		return fmt.Errorf("stop container: %w", err)
+	}
+	idLogger.Info("container stopped, now removing it")
+	if err := c.dc.ContainerRemove(ctx, containerID, types.ContainerRemoveOptions{}); err != nil {
+		return fmt.Errorf("stop container: %w", err)
+	}
+	idLogger.Info("container removed")
+	return nil
+}
+
+func (c *Client) ContainerExists(ctx context.Context, containerID string) (bool, error) {
+	idLogger := c.logger.With(zap.String("id", containerID))
+	idLogger.Info("checking if container exists by inspecting it")
+	_, err := c.dc.ContainerInspect(ctx, containerID)
+	if err != nil {
+		if dockerclient.IsErrContainerNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("container exists: %w", err)
+	}
+	return true, nil
+}
+
+func (c *Client) GetPortBinding(ctx context.Context, containerID string, port string) (string, error) {
+	idLogger := c.logger.With(
+		zap.String("id", containerID),
+	)
+	idLogger.Info("inspecting container")
+	inspectResponse, err := c.dc.ContainerInspect(ctx, containerID)
+	if err != nil {
+		return "", fmt.Errorf("get bound port: %w", err)
+	}
+	idLogger.Info("inspected container")
+	portMappings := inspectResponse.NetworkSettings.Ports[nat.Port(port)]
+	if len(portMappings) == 0 {
+		return "", fmt.Errorf("get bound port: no mappings for port %v", port)
+	}
+	hostPort := portMappings[0].HostPort
+	portBinding := fmt.Sprintf("%v:%v", c.host, hostPort)
+	idLogger.Info(
+		"found port mapping",
+		zap.String("port", port),
+		zap.String("portBinding", portBinding),
+	)
+	return portBinding, nil
 }
--- internal/dockertest/client_test.go
+++ internal/dockertest/client_test.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/Saser/strecku/internal/provide"
+	"github.com/cenkalti/backoff/v3"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 )
 
 func TestNewClient(t *testing.T) {
@@ -31,11 +35,99 @@ func TestNewClient(t *testing.T) {
 			if tt.valid {
 				require.NoError(t, err)
 				defer cleanup()
-				_, err := client.Ping(ctx)
+				_, err := client.dc.Ping(ctx)
 				require.NoError(t, err)
 			} else {
 				require.Error(t, err)
 			}
 		})
 	}
+}
+
+func TestClient_PullOfficialImage(t *testing.T) {
+	ctx := context.Background()
+	logger := provide.ZapTestLogger(t)
+	client, cleanup := client(ctx, t, logger)
+	defer cleanup()
+	for _, tt := range []struct {
+		image string
+		tag   string
+		valid bool
+	}{
+		{image: "hello-world", tag: "linux", valid: true},
+		{image: "invalid", tag: "invalid", valid: false},
+	} {
+		tt := tt
+		t.Run(fmt.Sprintf("image=%v,tag=%v", tt.image, tt.tag), func(t *testing.T) {
+			err := client.PullOfficialImage(ctx, tt.image, tt.tag)
+			if tt.valid {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
+
+func TestClient_StartContainer_StopContainer_ContainerExists(t *testing.T) {
+	ctx := context.Background()
+	logger := provide.ZapTestLogger(t)
+	client, cleanup := client(ctx, t, logger)
+	defer cleanup()
+	for _, tt := range []struct {
+		image string
+		tag   string
+		valid bool
+	}{
+		{image: "hello-world", tag: "linux", valid: true},
+		{image: "invalid", tag: "invalid", valid: false},
+	} {
+		tt := tt
+		t.Run(fmt.Sprintf("image=%v,tag=%v,valid=%v", tt.image, tt.tag, tt.valid), func(t *testing.T) {
+			id, err := client.StartContainer(ctx, tt.image, tt.tag)
+			if tt.valid {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				return
+			}
+			totalTimeout := 1 * time.Minute
+			stopTimeout := 10 * time.Second
+			stopCtx, stopCancel := context.WithTimeout(ctx, totalTimeout)
+			defer stopCancel()
+			operation := func() error { return client.StopContainer(stopCtx, id, stopTimeout) }
+			policy := backoff.WithContext(backoff.NewExponentialBackOff(), stopCtx)
+			err = backoff.Retry(operation, policy)
+			require.NoError(t, err)
+			exists, err := client.ContainerExists(ctx, id)
+			require.NoError(t, err)
+			require.False(t, exists)
+		})
+	}
+}
+
+func TestClient_GetPortBinding(t *testing.T) {
+	ctx := context.Background()
+	logger := provide.ZapTestLogger(t)
+	client, cleanup := client(ctx, t, logger)
+	defer cleanup()
+	id, err := client.StartContainer(ctx, "postgres", "11.5-alpine")
+	require.NoError(t, err)
+	stopTimeout := 10 * time.Second
+	defer func() {
+		err := client.StopContainer(ctx, id, stopTimeout)
+		require.NoError(t, err)
+	}()
+	address, err := client.GetPortBinding(ctx, id, "5432/tcp")
+	require.NoError(t, err)
+	assert.NotEmpty(t, address)
+	assert.Contains(t, address, ":")
+}
+
+func client(ctx context.Context, t *testing.T, logger *zap.Logger) (*Client, func()) {
+	c, cleanup, err := NewClient(logger)
+	require.NoError(t, err)
+	_, err = c.dc.Ping(ctx)
+	require.NoError(t, err)
+	return c, cleanup
 }
