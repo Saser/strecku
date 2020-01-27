5a9767373611d4898419c4f4baf3bde01b063120
Christian Persson <Saser@users.noreply.github.com>
2020-01-27 21:36:51 +0100

    backend: implement `StoreAPI` with an in-memory implementation
    This is very similar to the implementation of `UserAPI`, but even a
    little bit simpler.

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -66,12 +66,56 @@ func (u *UserAPIImpl) CreateUser(_ context.Context, req *streckuv1.CreateUserReq
 	}, nil
 }
 
+type StoreAPIImpl struct {
+	mu     sync.Mutex
+	stores map[string]*streckuv1.Store
+}
+
+func (s *StoreAPIImpl) GetStore(_ context.Context, req *streckuv1.GetStoreRequest) (*streckuv1.GetStoreResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	store, ok := s.stores[req.Name]
+	if !ok {
+		return nil, status.Error(codes.NotFound, "Store resource not found")
+	}
+	return &streckuv1.GetStoreResponse{
+		Store: store,
+	}, nil
+}
+
+func (s *StoreAPIImpl) ListStores(context.Context, *streckuv1.ListStoresRequest) (*streckuv1.ListStoresResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	stores := make([]*streckuv1.Store, 0, len(s.stores))
+	for _, store := range s.stores {
+		stores = append(stores, store)
+	}
+	return &streckuv1.ListStoresResponse{
+		Stores: stores,
+	}, nil
+}
+
+func (s *StoreAPIImpl) CreateStore(_ context.Context, req *streckuv1.CreateStoreRequest) (*streckuv1.CreateStoreResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	newStore := req.Store
+	newStore.Name = fmt.Sprintf("stores/%s", uuid.New().String())
+	s.stores[newStore.Name] = newStore
+	return &streckuv1.CreateStoreResponse{
+		Store: newStore,
+	}, nil
+}
+
 func main() {
-	impl := &UserAPIImpl{
+	userAPIImpl := &UserAPIImpl{
 		users: make(map[string]*streckuv1.User),
 	}
+	storeAPIImpl := &StoreAPIImpl{
+		stores: make(map[string]*streckuv1.Store),
+	}
 	server := grpc.NewServer()
-	streckuv1.RegisterUserAPIServer(server, impl)
+	streckuv1.RegisterUserAPIServer(server, userAPIImpl)
+	streckuv1.RegisterStoreAPIServer(server, storeAPIImpl)
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalf("error: %+v", err)
