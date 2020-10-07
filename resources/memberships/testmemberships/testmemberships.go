a0aeae8d367aec432fc78006b9491ae6f146511d
Christian Persson <Saser@users.noreply.github.com>
2020-10-07 21:39:11 +0200

    resources/memberships/testmemberships: add Discount field

--- resources/memberships/testmemberships/testmemberships.go
+++ resources/memberships/testmemberships/testmemberships.go
@@ -12,23 +12,27 @@ var (
 		User:          testusers.Alice.Name,
 		Store:         teststores.Bar.Name,
 		Administrator: false,
+		Discount:      false,
 	}
 	Alice_Mall = &pb.Membership{
 		Name:          "memberships/9681bce8-14da-4a11-a812-8245ccd1c911",
 		User:          testusers.Alice.Name,
 		Store:         teststores.Mall.Name,
 		Administrator: false,
+		Discount:      false,
 	}
 	Bob_Bar = &pb.Membership{
 		Name:          "memberships/ad8a0fc4-1482-4f00-b69c-f6d26104e504",
 		User:          testusers.Bob.Name,
 		Store:         teststores.Bar.Name,
 		Administrator: false,
+		Discount:      false,
 	}
 	Bob_Mall = &pb.Membership{
 		Name:          "memberships/70d30eac-d059-4712-9d16-f5ec5926d4f0",
 		User:          testusers.Bob.Name,
 		Store:         teststores.Mall.Name,
 		Administrator: false,
+		Discount:      false,
 	}
 )
